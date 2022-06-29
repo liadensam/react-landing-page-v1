84fbf4b967604bec9d56be292fe92b96d7ab22a1
liadensam <li.adensam@gmail.com>
2022-06-29 10:39:55 +0000

    update rel=noreferrer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className="footer">
         <p>© liadensam 2022</p>
-        <a className="link--footer" href="https://www.buymeacoffee.com/" target="_blind">Buy me a coffee :-)</a>
+        <a className="link--footer" href="https://www.buymeacoffee.com/" target="_blind" rel="noreferrer">Buy me a coffee :-)</a>
     </div>
   );
 };
